390804111ff116585f912c5a62645ccaf88019b6
Kuntida-1 <kuntida.y@ku.th>
2025-07-27 08:32:01 +0000

    Url

--- src/shop.jsx
+++ src/shop.jsx
@@ -1,9 +1,8 @@
-import { useState } from "react";
+import './Shop.css'
+import { useState, useEffect } from "react";
 export default function Shop(){
-
-    const book=[
-        
-        { "id": 1, "title": "The Let Them Theory: A Life-Changing Tool That Millions of People Can't Stop Talking About", "author": "Mel Robbins", "image_url": "https://images-na.ssl-images-amazon.com/images/I/91I1KDnK1kL._AC_UL381_SR381,381_.jpg", "price": 11.69 },
+    const [books,setBooks]= useState([
+         { "id": 1, "title": "The Let Them Theory: A Life-Changing Tool That Millions of People Can't Stop Talking About", "author": "Mel Robbins", "image_url": "https://images-na.ssl-images-amazon.com/images/I/91I1KDnK1kL._AC_UL381_SR381,381_.jpg", "price": 11.69 },
         { "id": 2, "title": "Forgotten Home Apothecary : 250 Powerful Remedies at Your Fingertips", "author": "Dr. Nicole Apelian", "image_url": "https://images-na.ssl-images-amazon.com/images/I/91-E86oM2IL._AC_UL381_SR381,381_.jpg", "price": 37 },
         { "id": 3, "title": "Seven Things You Can't Say About China", "author": "Tom Cotton", "image_url": "https://images-na.ssl-images-amazon.com/images/I/81+mN748qkL._AC_UL381_SR381,381_.jpg", "price": 19.58 },
         { "id": 4, "title": "Atomic Habits: An Easy & Proven Way to Build Good Habits & Break Bad Ones", "author": "James Clear", "image_url": "https://images-na.ssl-images-amazon.com/images/I/81ANaVZk5LL._AC_UL381_SR381,381_.jpg", "price": 14.49 },
@@ -11,10 +10,28 @@ export default function Shop(){
         { "id": 6, "title": "I Wish Someone Had Told Me . . .: The Best Advice for Building a Great Career and a Meaningful Life", "author": "Dana Perino", "image_url": "https://images-na.ssl-images-amazon.com/images/I/81AOHbxGNfL._AC_UL254_SR254,254_.jpg", "price": 20.30 },
         { "id": 7, "title": "How to Giggle: A Guide to Taking Life Less Seriously", "author": "Hannah Berner", "image_url": "https://images-na.ssl-images-amazon.com/images/I/81rO3vvG1mL._AC_UL254_SR254,254_.jpg", "price": 20.29 },
         { "id": 8, "title": "Strangers in Time: A World War II Novel", "author": "David Baldacci", "image_url": "https://images-na.ssl-images-amazon.com/images/I/816QI0pfuRL._AC_UL254_SR254,254_.jpg", "price": 17.84 }
-    ];
+        
+    ]);
+    const url="https://fluffy-carnival-q7vxgrv44624pv5-5001.app.github.dev/books";
+    useEffect(()=>{
+        async function fetchData() {
+            try{
+                    const response = await fetch(url)
+                    if(response.ok){
+                        console.log('Connect to backend succesfully')
+                        const data = await response.json();
+                        setBooks(data.books);
+                    }else throw new Error('Failed to fetch data')
+            }catch(error){
+                console.error('Eror:',error);
+            }
+        }
+        fetchData();
+    })
+   
     const[query,setQuety]=useState("");
     const[cart,setCart]=useState([]);
-    const filterlist=book.filter(b=>
+    const filterlist=books.filter(b=>
         b.title.toLocaleLowerCase().includes(query.toLocaleLowerCase()) ||
         b.author.toLocaleLowerCase().includes(query.toLocaleLowerCase())
     );
